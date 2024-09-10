8aee197765b4b13628f5ff108b049e9b6c440408
Lincoln Rebouças <lincoln.reboucas40@gmail.com>
2024-09-10 17:07:52 -0300

    questao 5

---
+++ quest5.py
@@ -0,0 +1,15 @@
+# 5) Escreva um programa que inverta os caracteres de um string.
+
+# IMPORTANTE:
+# a) Essa string pode ser informada através de qualquer entrada de 
+# sua preferência ou pode ser previamente definida no código;
+# b) Evite usar funções prontas, como, por exemplo, reverse;
+
+def inverter_string(s):
+    invertida = ""
+    for i in range(len(s) - 1, -1, -1):
+        invertida += s[i]
+    return invertida
+
+string = input("Informe uma string: ")
+print(f"String invertida: {inverter_string(string)}")
